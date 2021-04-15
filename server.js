2256606022a4966ea0804fa3247dddde54a8e773
nitalpatel <72319223+nitalpatel@users.noreply.github.com>
2021-04-14 23:59:51 -0500

    unfinished commits

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const compression = require("compression");
+
+var PORT = process.env.PORT || 8080;
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(compression());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+  mongoose.connect(
+    process.env.MONGODB_URI || "mongodb://localhost/budget", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+});
+
+// routes
+app.use(require("./routes/api.js"));
+// require("./routes/api.js")(app);
+
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
+
+
